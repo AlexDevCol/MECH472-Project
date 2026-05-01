898112f846b017ad1a57fe685d278678ff2770ce
AlexDevCol <luigi.gonzalez@live.com>
2026-05-01 17:53:27 -0400

    Implemented vision

--- src/Project DirectX Image View Bridge/robot_fight_simulator/draw_3D_graphics.cpp
+++ src/Project DirectX Image View Bridge/robot_fight_simulator/draw_3D_graphics.cpp
@@ -31,6 +31,14 @@
 
 const double PI = 4*atan(1.0);
 
+#include "vision.h"
+
+#include "my_functions.h"
+
+void find_robots(int nblobs, double** blob_matrix, double robot_ic[2], double robot_jc[2], double robot_theta[2], bool robot_found[2]);
+
+bool UseCamera = false;
+
 #define SQR(x)	((x)*(x))
 
 // dynamic arrays for storing DirectX vertices
@@ -109,7 +117,9 @@ static ofstream fout("timing.txt");
 
 world* pWorld = 0;
 
-image b;
+int nblobs;
+image b, rgb0, label; // declare some image structures
+image grey_image;
 
 void draw_3D_graphics()
 {
@@ -122,6 +132,18 @@ void draw_3D_graphics()
 		b.height = WINDOW_HEIGHT;
 		b.type = RGB_IMAGE;
 		allocate_image(b);
+		rgb0.width = WINDOW_WIDTH;
+		rgb0.height = WINDOW_HEIGHT;
+		rgb0.type = RGB_IMAGE;
+		allocate_image(rgb0);
+		label.width = WINDOW_WIDTH;
+		label.height = WINDOW_HEIGHT;
+		label.type = LABEL_IMAGE;
+		allocate_image(label);
+		grey_image.width = WINDOW_WIDTH;
+		grey_image.height = WINDOW_HEIGHT;
+		grey_image.type = GREY_IMAGE;
+		allocate_image(grey_image);
 
 		// Robot sprites (BMPs copied to OutDir by post-build step)
 		create_sprite_2D("robot_A.bmp", 0);
@@ -143,8 +165,151 @@ void draw_3D_graphics()
 
 	screen_grab((unsigned char *)(b.pdata),
 		(int)(b.width), (int)(b.height));
+
+	copy(b, rgb0);
+
+    nblobs = process_image(b, label);
+
+    copy(rgb0, b);
+
+
+
+    /////////////////////////////////////////////// Find Robots ///////////////////////////////////////////////////////
+    // Allocate memory for blob analysis
+    double** blob_matrix = new double* [nblobs];
+    for (int i = 0; i < nblobs; i++) {
+        blob_matrix[i] = new double[5];
+    }
+
+
+    // Find/Process average color and display centorid of that color
+    double R_ave, G_ave, B_ave;
+    double ic, jc;
+    copy(b, grey_image);
+
+    for (int i = 1; i <= nblobs; i++) {
+        // use the original colour image rgb0 here
+        centroid(grey_image, label, i, ic, jc);
+        average_colour(rgb0, label, i, R_ave, G_ave, B_ave);
+
+        cout << "\nBlob #" << i;
+        cout << "\nR_ave = " << R_ave;
+        cout << "\nG_ave = " << G_ave;
+        cout << "\nB_ave = " << B_ave;
+
+        // mark the centroid point on the image with a colored point
+        draw_point_rgb(b, (int)ic, (int)jc, R_ave, G_ave, B_ave);
+
+        // Store in the matrix (remember matrix index is 0-based, blob loop is 1-based)
+        blob_matrix[i - 1][0] = ic;
+        blob_matrix[i - 1][1] = jc;
+        blob_matrix[i - 1][2] = R_ave;
+        blob_matrix[i - 1][3] = G_ave;
+        blob_matrix[i - 1][4] = B_ave;
+    }
+
+    // Prepare Output arrays
+    double robot_centers_i[2];
+    double robot_centers_j[2];
+    double robot_angles[2];
+    bool robot_status[2];
+
+    find_robots(nblobs, blob_matrix, robot_centers_i, robot_centers_j, robot_angles, robot_status);
+
+    if (robot_status[0]) {
+        cout << "\nRed-Green Robot: (" << robot_centers_i[0] << ", " << robot_centers_j[0] << ") at " << robot_angles[0] << " deg";
+    }
+    draw_point_rgb(b, (int)robot_centers_i[0], (int)robot_centers_j[0], 255, 182, 193);
+    if (robot_status[1]) {
+        cout << "\nBlue-Orange Robot: (" << robot_centers_i[1] << ", " << robot_centers_j[1] << ") at " << robot_angles[1] << " deg";
+    }
+    draw_point_rgb(b, (int)robot_centers_i[1], (int)robot_centers_j[1], 255, 182, 193);
+
+
+    // Must delete the columns first, then the rows!
+    for (int i = 0; i < nblobs; i++) {
+        delete[] blob_matrix[i];
+    }
+    delete[] blob_matrix;
+
+    cout << "\nAverage color centroids displayed";
+
+    /////////////////////////////////////////////// Find Robots ///////////////////////////////////////////////////////
+
 	view_rgb_image(b);
 }
 
+
+
+// Function to find robots using a 2D dynamic matrix.
+// Matrix structure per row: [0] = ic, [1] = jc, [2] = R, [3] = G, [4] = B
+void find_robots(int nblobs, double** blob_matrix, double robot_ic[2], double robot_jc[2], double robot_theta[2], bool robot_found[2])
+{
+    robot_found[0] = false;
+    robot_found[1] = false;
+
+    double red_i = -1, red_j = -1;
+    double green_i = -1, green_j = -1;
+    double blue_i = -1, blue_j = -1;
+    double orange_i = -1, orange_j = -1;
+
+    double hue, sat, value;
+
+    // 1. Loop through the rows of the matrix
+    for (int k = 0; k < nblobs; k++) {
+        // Extract data from the matrix for readability
+        double ic = blob_matrix[k][0];
+        double jc = blob_matrix[k][1];
+        int R = (int)blob_matrix[k][2];
+        int G = (int)blob_matrix[k][3];
+        int B = (int)blob_matrix[k][4];
+
+        // Convert to HSV
+        calculate_HSV(R, G, B, hue, sat, value);
+
+        /*
+        Red: hue > 340 OR hue < 20
+        Orange: hue > 20 AND hue < 45
+        Green: hue > 90 AND hue < 170
+        Blue: hue > 200 AND hue < 260
+        */
+
+        if (sat > 0.3) { // Discard grey marks
+            // Classify by Hue Ranges
+            if (hue < 20 || hue > 340) {
+                red_i = ic; red_j = jc;
+                cout << "\nFound red centroid!";
+            }
+            else if (hue >= 20 && hue <= 60) {
+                orange_i = ic; orange_j = jc;
+                cout << "\nFound orange centroid!";
+            }
+            else if (hue >= 90 && hue <= 170) {
+                green_i = ic; green_j = jc;
+                cout << "\nFound green centroid!";
+            }
+            else if (hue >= 200 && hue <= 260) {
+                blue_i = ic; blue_j = jc;
+                cout << "\nFound blue centroid!";
+            }
+        }
+    }
+
+    // 2. Robot 0 (Red/Green)
+    if (red_i != -1 && green_i != -1) {
+        robot_found[0] = true;
+        robot_ic[0] = (red_i + green_i) / 2.0;
+        robot_jc[0] = (red_j + green_j) / 2.0;
+        robot_theta[0] = atan2(green_j - red_j, green_i - red_i) * (180.0 / 3.14159);
+    }
+
+    // 3. Robot 1 (Blue/Orange)
+    if (blue_i != -1 && orange_i != -1) {
+        robot_found[1] = true;
+        robot_ic[1] = (blue_i + orange_i) / 2.0;
+        robot_jc[1] = (blue_j + orange_j) / 2.0;
+        robot_theta[1] = atan2(orange_j - blue_j, orange_i - blue_i) * (180.0 / 3.14159);
+    }
+}
 
 
---
+++ src/Project DirectX Image View Bridge/robot_fight_simulator/my_functions.h
@@ -0,0 +1,12 @@
+#pragma once
+
+int load_image(image& rgb, image& rgb0, bool useCamera = false, int cam_number = 0);
+int process_image(image& rgb, image& label, bool draw_ics = false, bool light_bg = false, image* segmented_grey_out = nullptr);
+
+void calculate_HSV(int R, int G, int B, double& hue, double& sat, double& value);
+void calculate_hue_image(image& rgb, image& hue_image);
+
+void display_sobel_images(image& grey_image);
+int sobel(image& a, image& mag, image& theta);
+
+void average_colour(image& rgb, image& label_image, int label_num, double& R, double& G, double& B);
